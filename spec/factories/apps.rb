109d45fe646c0b87408cee8c3f6cb5397f6e567a
Backendevloper <chandrasekharjangamn@gmail.com>
2024-10-04 16:36:10 +0530

    final commit

---
+++ spec/factories/apps.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :app do
+    slack_id { "" }
+    auth_user_id  { '' }
+    scope  { "" }
+    team_id { "" }
+    team_name {  "" }
+    access_token { "" }
+  end
+end 
